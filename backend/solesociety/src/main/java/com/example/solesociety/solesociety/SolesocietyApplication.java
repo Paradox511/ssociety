76aae076e858d168723f3aa1e1e9fbd7964fe1cb
Duc <ADMIN@MSI>
2025-09-22 10:37:28 +0700

    initial

---
+++ backend/solesociety/src/main/java/com/example/solesociety/solesociety/SolesocietyApplication.java
@@ -0,0 +1,13 @@
+package com.example.solesociety.solesociety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolesocietyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolesocietyApplication.class, args);
+	}
+
+}
---
+++ backend/solesociety/src/test/java/com/example/solesociety/solesociety/SolesocietyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solesociety.solesociety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolesocietyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
